5e1e267a2eb8cbb01a8f49530b9c2dcaabd7e1a6
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-02-02 17:40:53 +0630

    update: settings

--- CiviCareManagementSystem/settings.py
+++ CiviCareManagementSystem/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',), 
 }
