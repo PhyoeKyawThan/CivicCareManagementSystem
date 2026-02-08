5865e3dd2ca9e01dc7bd40b37d52e85d7c22ad27
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-02-09 00:26:10 +0630

    fix: missing django setting in asgi left without setting django setup

--- CiviCareManagementSystem/asgi.py
+++ CiviCareManagementSystem/asgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CiviCareManagementSystem.settings')
+
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import notifications.routing
 from notifications.middleware import JWTAuthMiddleware
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CiviCareManagementSystem.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
